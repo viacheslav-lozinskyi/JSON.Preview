f2f96cfd36cf589f6aa6759e3819366a029674d0
Viacheslav Lozinskyi <viacheslav.lozinskyi@gmail.com>
2022-08-14 09:58:29 +0300

    - Minor improvements

--- application/preview-json.vs/resource/package/VSPackage.cs
+++ application/preview-json.vs/resource/package/VSPackage.cs
@@ -24,7 +24,7 @@ namespace resource.package
             public const string DESCRIPTION = "Quick preview of JSON files";
             public const string GUID = "9E4EBB7D-C389-407B-900F-ADCD2E3F1B80";
             public const string NAME = "Preview-JSON";
-            public const string VERSION = "1.0.11";
+            public const string VERSION = "1.0.12";
         }
 
         protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
