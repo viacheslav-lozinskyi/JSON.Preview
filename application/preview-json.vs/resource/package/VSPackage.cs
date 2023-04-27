0bf0f7b9cd485278223a30d88338114c2de24544
Viacheslav Lozinskyi <viacheslav.lozinskyi@gmail.com>
2023-04-27 17:54:41 +0300

    - Minor changes

--- application/preview-json.vs/resource/package/VSPackage.cs
+++ application/preview-json.vs/resource/package/VSPackage.cs
@@ -20,6 +20,7 @@ namespace resource.package
         internal static class CONSTANT
         {
             public const string APPLICATION = "Visual Studio";
+            public const string COMPANY = "Viacheslav Lozinskyi";
             public const string COPYRIGHT = "Copyright (c) 2020-2023 by Viacheslav Lozinskyi. All rights reserved.";
             public const string DESCRIPTION = "Quick preview of JSON files";
             public const string GUID = "9E4EBB7D-C389-407B-900F-ADCD2E3F1B80";
